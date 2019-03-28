9917b258ff04b2f7f687fb57b8bad1684cbe97a4
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2019-03-28 22:21:27 +0900

    add tv

---
+++ tv-example/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  tv-example
+//
+//  Created by Kazuaki Matsuo on 2019/03/22.
+//  Copyright © 2019 Kazuaki Matsuo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tv-example/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  tv-example
+//
+//  Created by Kazuaki Matsuo on 2019/03/22.
+//  Copyright © 2019 Kazuaki Matsuo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
